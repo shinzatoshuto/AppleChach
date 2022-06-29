8561bdd3179081abd5a68c48ba8c0210319f5e45
Shideken <106945368+Shideken@users.noreply.github.com>
2022-06-30 02:36:12 +0900

    playerimages

--- apple chach/apple chach/Player.h
+++ apple chach/apple chach/Player.h
@@ -1,18 +1,2 @@
 #pragma once
 
-//class Player {
-//public:
-//	int flg;				// 使用フラグ
-//	int x, y;				// 座標x, y
-//	int w, h;				// 幅 w 高さ h
-//	double angle;			// 機体の向き 
-//	int count;				/// タイミング用
-//	int point;
-//	int speed;
-//	int A, B, C, D;
-//	int charaimgs[4];
-//
-//public:
-//	void PlayerControl();
-//};
-//extern Player player;
--- apple chach/apple chach/Playercon.cpp
+++ apple chach/apple chach/Playercon.cpp
@@ -3,11 +3,3 @@
 #include"hensuu.h"
 
 
-//void Player::PlayerControl(void) {
-//	if (player.flg == true)
-//	{
-//		if (hen.g_NowKey & PAD_INPUT_LEFT)player.x -= player.speed;
-//		if (hen.g_NowKey & PAD_INPUT_RIGHT)player.x += player.speed;
-//	}
-//
-//}

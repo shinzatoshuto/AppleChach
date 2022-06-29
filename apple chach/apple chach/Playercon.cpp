81407e55f756671557523e7398b2fe594c53b751
Shideken <106945368+Shideken@users.noreply.github.com>
2022-06-30 01:42:42 +0900

    p

--- apple chach/apple chach/Playercon.cpp
+++ apple chach/apple chach/Playercon.cpp
@@ -6,8 +6,8 @@
 void Player::PlayerControl(void) {
 	if (player.flg == true)
 	{
-		if (hen.g_NowKey & PAD_INPUT_LEFT)g_player.x -= g_player.speed;
-		if (hen.g_NowKey & PAD_INPUT_RIGHT)g_player.x += g_player.speed;
+		if (hen.g_NowKey & PAD_INPUT_LEFT)player.x -= player.speed;
+		if (hen.g_NowKey & PAD_INPUT_RIGHT)player.x += player.speed;
 	}
 
 }

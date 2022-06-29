a614633688b547f9b39e61994dbfb6af0dea9046
Shideken <106945368+Shideken@users.noreply.github.com>
2022-06-30 00:40:08 +0900

    a

--- apple chach/apple chach/Chara.h
+++ apple chach/apple chach/Chara.h
@@ -1,1 +1,16 @@
 #pragma once
+
+class Chara {
+public:
+	int x, y, w, h;
+	int type;
+	int flg;
+	int img;
+	int point;
+	int speed;
+	int A, B, C, D;
+	int charaimgs[4];
+
+public:
+	void PlayerControl();
+};

2dc24fea5da0bef08ad0eec63b315b998ed18841
Shideken <106945368+Shideken@users.noreply.github.com>
2022-06-30 01:41:45 +0900

    p

--- apple chach/apple chach/Playercon.cpp
+++ apple chach/apple chach/Playercon.cpp
@@ -1,9 +1,10 @@
 #include "DxLib.h"
-#include "Chara.h"
+#include "Player.h"
 #include"hensuu.h"
 
-void Chara::PlayerControl(void) {
-	if (g_player.flg == true)
+
+void Player::PlayerControl(void) {
+	if (player.flg == true)
 	{
 		if (hen.g_NowKey & PAD_INPUT_LEFT)g_player.x -= g_player.speed;
 		if (hen.g_NowKey & PAD_INPUT_RIGHT)g_player.x += g_player.speed;

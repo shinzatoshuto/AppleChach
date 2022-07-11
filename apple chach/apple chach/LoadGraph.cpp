19183be37f3167579d913c15ee96a5d2a620534b
shinzatoshuto <syuto-shinzato-k21h@stu.kbc.ac.jp>
2022-07-11 11:01:13 +0900

    gazou

--- apple chach/apple chach/LoadGraph.cpp
+++ apple chach/apple chach/LoadGraph.cpp
@@ -50,11 +50,11 @@ int LoadImages() {
 		if ((hen.EndImg = LoadGraph("images/back/bg_yamamichi.jpg")) == -1)return-1;
 
 		//•¶Žš‰æ‘œ
-		if (LoadDivGraph("images/ALAN.png", 13, 13, 1, 45, 80, pad.ALAN) == -1)return-1;
-		if (LoadDivGraph("images/ALOZ.png", 12, 12, 1, 45, 80, pad.ALOZ) == -1)return-1;
-		if (LoadDivGraph("images/ALAN_ko.png", 13, 13, 1, 49, 90, pad.alan) == -1)return-1;
-		if (LoadDivGraph("images/ALOZ_ko.png", 12, 12, 1, 45, 82, pad.aloz) == -1)return-1;
-		if (LoadDivGraph("images/suuji.png", 10, 10, 1, 44, 74, pad.suuji) == -1)return-1;
+		if (LoadDivGraph("images/ReALAN.png", 14, 14, 1, 45, 80, pad.ALAN) == -1)return-1;
+		if (LoadDivGraph("images/ReALOZ.png", 12, 12, 1, 45, 80, pad.ALOZ) == -1)return-1;
+		if (LoadDivGraph("images/ReALAN_ko.png", 13, 13, 1, 49, 90, pad.alan) == -1)return-1;
+		if (LoadDivGraph("images/ReALOZ_ko.png", 12, 12, 1, 45, 82, pad.aloz) == -1)return-1;
+		if (LoadDivGraph("images/Resuuji.png", 10, 10, 1, 44, 74, pad.suuji) == -1)return-1;
 	
 	return 0;
 }
--- apple chach/apple chach/pad.h
+++ apple chach/apple chach/pad.h
@@ -2,7 +2,7 @@
 
 class PAD {
 public:
-	int ALAN[13], ALOZ[12], alan[13], aloz[12], suuji[10];
+	int ALAN[14], ALOZ[12], alan[13], aloz[12], suuji[10];
 };
 
 extern PAD pad;

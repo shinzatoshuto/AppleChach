cc50c7fbf9f64c1055232d866d914c3ad8268b18
Shideken <106945368+Shideken@users.noreply.github.com>
2022-07-12 09:49:24 +0900

    Help

--- apple chach/apple chach/main.cpp
+++ apple chach/apple chach/main.cpp
@@ -4,6 +4,7 @@
 #include"apple.h"
 #include "hensuu.h"
 #include "Font.h"
+#include"Help.h"
 #include"pad.h"
 
 #define  RANKING_DATA  5
@@ -12,6 +13,7 @@ APPLE apple[APPLE_MAX];
 HENSUU hen;
 PAD pad;
 FONT font;
+HELP help;
 
 //定数の宣言
 //const int SCREEN_WIDTH = 640;
@@ -184,7 +186,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR IpCmdLine
 			break;
 		case 2:DrawRanking();
 			break;
-		case 3:DrawHelp();
+		case 3:help.DrawHelp();
 			break;
 		case 4:DrawEnd();
 			break;
@@ -283,38 +285,38 @@ void DrawRanking(void) {
 }
 
 ////ゲームヘルプ描画処理
-void DrawHelp(void) {
-	//スペースキーでメニューに戻る
-	if (hen.g_KeyFlg & PAD_INPUT_2) hen.g_GameState = 0;
-	//zゲーム開始
-	if (hen.g_KeyFlg & PAD_INPUT_1) hen.g_GameState = 1;
-
-	//タイトル画像表示
-	DrawGraph(0, 0, hen.HelpImage, FALSE);
-	SetFontSize(16);
-
-	DrawString(20, 160, "これは落ちてくるリンゴを拾うゲームです。", 0x000000, 0);
-	DrawString(20, 180, "左右に動いて落ちてくる様々なリンゴを拾います。", 0x000000, 0);
-	DrawString(20, 200, "※上下移動はできません", 0x000000, 0);
-	DrawString(20, 220, "制限時間は30秒です。", 0x000000, 0);
-	DrawString(20, 250, "リンゴ一覧", 0x000000, 0);
-	DrawGraph(20, 260, hen.AppleImages[0], TRUE);
-	DrawString(62, 275, "+150P", 0x0000ff, 0);
-	DrawGraph(120, 260, hen.AppleImages[1], TRUE);
-	DrawString(162, 275, "+300P", 0x0000ff, 0);
-	DrawGraph(220, 260, hen.AppleImages[2], TRUE);
-	DrawString(262, 275, "+500P", 0x0000ff, 0);
-	DrawString(20, 315, "赤、黄、緑色のリンゴをとるとスコアが増えるよ。", 0x000000, 0);
-	DrawGraph(20, 335, hen.AppleImages[3], TRUE);
-	DrawString(60, 350, "-1000P", 0xff0000, 0);
-	DrawString(20, 385, "毒リンゴに触れるとすこあが減るよ。", 0x000000, 0);
-	DrawString(20, 425, "プレイ中にSTARTボタンを押すとポーズに移行します。", 0x000000, 0);
-	DrawString(20, 445, "(もう一度押すとプレイ画面に戻ります)", 0x000000, 0);
-	DrawString(490, 450, "ゲームへ[Aボタン]", 0x000000, 0);
-	DrawString(524, 430, "戻る[Bボタン]", 0x000000, 0);
-
-	DrawStringToHandle(20, 120, "ヘルプ画面", 0xffffff, font.font00);
-}
+//void DrawHelp(void) {
+//	//スペースキーでメニューに戻る
+//	if (hen.g_KeyFlg & PAD_INPUT_2) hen.g_GameState = 0;
+//	//zゲーム開始
+//	if (hen.g_KeyFlg & PAD_INPUT_1) hen.g_GameState = 1;
+//
+//	//タイトル画像表示
+//	DrawGraph(0, 0, hen.HelpImage, FALSE);
+//	SetFontSize(16);
+//
+//	DrawString(20, 160, "これは落ちてくるリンゴを拾うゲームです。", 0x000000, 0);
+//	DrawString(20, 180, "左右に動いて落ちてくる様々なリンゴを拾います。", 0x000000, 0);
+//	DrawString(20, 200, "※上下移動はできません", 0x000000, 0);
+//	DrawString(20, 220, "制限時間は30秒です。", 0x000000, 0);
+//	DrawString(20, 250, "リンゴ一覧", 0x000000, 0);
+//	DrawGraph(20, 260, hen.AppleImages[0], TRUE);
+//	DrawString(62, 275, "+150P", 0x0000ff, 0);
+//	DrawGraph(120, 260, hen.AppleImages[1], TRUE);
+//	DrawString(162, 275, "+300P", 0x0000ff, 0);
+//	DrawGraph(220, 260, hen.AppleImages[2], TRUE);
+//	DrawString(262, 275, "+500P", 0x0000ff, 0);
+//	DrawString(20, 315, "赤、黄、緑色のリンゴをとるとスコアが増えるよ。", 0x000000, 0);
+//	DrawGraph(20, 335, hen.AppleImages[3], TRUE);
+//	DrawString(60, 350, "-1000P", 0xff0000, 0);
+//	DrawString(20, 385, "毒リンゴに触れるとすこあが減るよ。", 0x000000, 0);
+//	DrawString(20, 425, "プレイ中にSTARTボタンを押すとポーズに移行します。", 0x000000, 0);
+//	DrawString(20, 445, "(もう一度押すとプレイ画面に戻ります)", 0x000000, 0);
+//	DrawString(490, 450, "ゲームへ[Aボタン]", 0x000000, 0);
+//	DrawString(524, 430, "戻る[Bボタン]", 0x000000, 0);
+//
+//	DrawStringToHandle(20, 120, "ヘルプ画面", 0xffffff, font.font00);
+//}
 
 
 //ゲームエンド描画処理
